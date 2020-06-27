a47b74f2f79f8ab2c26297d3ee6f12e1876e351c
Wei Gao <wei@gaofamily.org>
2020-06-27 13:10:19 -0700

    Use -P to specify property

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./gradlew publish -Dsigning.gnupg.keyName="${GPG_KEY_NAME}" -Dsigning.gnupg.passphrase="${GPG_PASSPHRASE}"
+    ./gradlew publish -Psigning.gnupg.keyName="${GPG_KEY_NAME}" -Psigning.gnupg.passphrase="${GPG_PASSPHRASE}"
 fi
