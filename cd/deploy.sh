620c2f91af3619e23c39077ce2b6763894d032be
Wei Gao <wei@gaofamily.org>
2020-06-27 15:08:07 -0700

    Still need passphrase.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./gradlew publish -Psigning.gnupg.keyName="03EB73BF" -Psigning.gnupg.useLegacyGpg=true
+    ./gradlew publish -Psigning.gnupg.keyName="03EB73BF" -Psigning.gnupg.passphrase="${GPG_PASSPHRASE}" -Psigning.gnupg.useLegacyGpg=true
 fi
