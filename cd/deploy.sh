c134c17914fe77280adb2d9af6eed6cd49ee3c16
Wei Gao <wei@gaofamily.org>
2020-06-27 12:56:55 -0700

    Play with signing plugin

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./gradlew publish
+    ./gradlew publish -Dsigning.gnupg.keyName="${GPG_KEY_NAME}" -Dsigning.gnupg.passphrase="${GPG_PASSPHRASE}"
 fi
