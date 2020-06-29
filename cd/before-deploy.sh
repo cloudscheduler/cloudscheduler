2032ab138d7f606d0612933abb30c27da1af7131
Wei Gao <wei@gaofamily.org>
2020-06-28 20:41:36 -0700

    Prepare for v1.0.3 release

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" == 'false' && "$TRAVIS_BRANCH" == 'master' ]]; then
     openssl aes-256-cbc -K $encrypted_ede68231167e_key -iv $encrypted_ede68231167e_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
